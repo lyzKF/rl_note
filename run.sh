efe91b1c52ff7144739c7e8b842f948d875cedcf
lyz <32644102+lyzKF@users.noreply.github.com>
2025-09-16 11:33:14 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ if [ ! -n "${script}" ] ;then
     echo "need script info, eg: sh run.sh classic_control.cart_pole.cart_pole_dqn"
     exit
 else
-    /Users/guoliang21/miniforge3/bin/python3 -m ${script}
+    python3 -m ${script}
 fi
